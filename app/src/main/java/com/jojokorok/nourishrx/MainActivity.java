31fc96c7ffb2c42e36a6a62c54f8324113fdad29
JoJoKorok <50675784+JoJoKorok@users.noreply.github.com>
2026-08-15 23:02:26 -0400

    Redesign medication management screens

--- app/src/main/java/com/jojokorok/nourishrx/MainActivity.java
+++ app/src/main/java/com/jojokorok/nourishrx/MainActivity.java
@@ -426,21 +426,6 @@ public class MainActivity extends Activity {
             }
 
             @Override
-            public String selectedProfileName() {
-                return MainActivity.this.selectedProfileName();
-            }
-
-            @Override
-            public View sectionTitle(String title, String subtitle) {
-                return MainActivity.this.sectionTitle(title, subtitle);
-            }
-
-            @Override
-            public void emptyState(String message, String action, View.OnClickListener listener) {
-                MainActivity.this.emptyState(message, action, listener);
-            }
-
-            @Override
             public void showMedicationDialog(Medication medication) {
                 medicationEditorFlow.show(medication);
             }
